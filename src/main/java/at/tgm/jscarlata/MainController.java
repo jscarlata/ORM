32e71ecdd6ab8bdbc355777c179302e38fc306f9
jscarlata <jscarlata@student.tgm.ac.at>
2024-05-28 17:46:00 +0200

    Ekue

--- src/main/java/at/tgm/jscarlata/MainController.java
+++ src/main/java/at/tgm/jscarlata/MainController.java
@@ -3,8 +3,12 @@ package at.tgm.jscarlata;
 import at.tgm.jscarlata.warehouse.WarehouseData;
 import at.tgm.jscarlata.warehouse.WarehouseSimulation;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
+import at.tgm.jscarlata.warehouse.Product;
+
+import java.util.Optional;
 
 @Component
 @RestController
@@ -24,4 +28,41 @@ public class MainController {
     public @ResponseBody Iterable<WarehouseData> getAllWarehouses() {
         return warehouseRepository.findAll();
     }
+
+    @GetMapping(path="/warehouse/{id}")
+    public ResponseEntity<WarehouseData> getWarehouseById(@PathVariable int id) {
+        Optional<WarehouseData> warehouseData = Optional.ofNullable(warehouseRepository.findById(id));
+        return warehouseData.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping(path="/warehouse/{warehouseId}/product/{productId}")
+    public ResponseEntity<Product> getProductById(@PathVariable int warehouseId, @PathVariable int productId) {
+        Optional<WarehouseData> warehouseData = Optional.ofNullable(warehouseRepository.findById(warehouseId));
+        if (warehouseData.isPresent()) {
+            for (Product product : warehouseData.get().getProductData()) {
+                if (product.getId() == productId) {
+                    return ResponseEntity.ok(product);
+                }
+            }
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping(path="/warehouse/{id}")
+    public ResponseEntity<WarehouseData> updateWarehouse(@PathVariable int id, @RequestBody WarehouseData newWarehouseData) {
+        Optional<WarehouseData> optionalWarehouseData = Optional.ofNullable(warehouseRepository.findById(id));
+        return optionalWarehouseData.map(warehouseData -> {
+            warehouseData.setName(newWarehouseData.getName());
+            warehouseData.setTimestamp(newWarehouseData.getTimestamp());
+            warehouseData.setStreet(newWarehouseData.getStreet());
+            warehouseData.setCity(newWarehouseData.getCity());
+            warehouseData.setCountry(newWarehouseData.getCountry());
+            warehouseData.setPlz(newWarehouseData.getPlz());
+            warehouseData.setProductData(newWarehouseData.getProductData());
+            warehouseRepository.save(warehouseData);
+            return ResponseEntity.ok(warehouseData);
+        }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
 }
--- src/main/java/at/tgm/jscarlata/WarehouseRepository.java
+++ src/main/java/at/tgm/jscarlata/WarehouseRepository.java
@@ -4,5 +4,5 @@ import at.tgm.jscarlata.warehouse.WarehouseData;
 import org.springframework.data.repository.CrudRepository;
 
 public interface WarehouseRepository extends CrudRepository<WarehouseData, Integer> {
-
+    WarehouseData findById(int id);
 }
